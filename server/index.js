ab9fb5401e4185b7434e1e64dc7cac0ab45b7675
DinhAnnh <96048192+DinhAnnh@users.noreply.github.com>
2025-11-08 00:13:03 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,22 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  'https://nha-tro-two.vercel.app', // domain FE trên Vercel
+  'http://localhost:5173',     // để test local
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
